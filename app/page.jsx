8b2c44a09622e99e2137b476b64767b5025e5ddf
Theodore Chan <theodore@Theos-MacBook.local>
2025-10-30 10:14:02 -0400

    fixing now desktop landing page

--- app/page.jsx
+++ app/page.jsx
@@ -178,17 +178,21 @@ export default function Home() {
           height={120}
         />
       </div>
-      <div className="absolute w-full md:max-w-2xl text-5xl scale-95 text-black text-center left-1/2 -translate-x-1/2 font-bold top-[700px] md:top-[1120px]">
+      <div className="absolute w-full md:max-w-2xl text-4xl md:text-5xl scale-95 text-black text-center left-1/2 -translate-x-1/2 font-bold top-[700px] md:top-[1120px]">
         A place to stay connected with online communities <br /> Build together.
         Learn together. <br /> Be together.
       </div>
       <div className="absolute w-full md:w-3xl scale-95 text-black text-center left-1/2 -translate-x-1/2 font-bold top-[1120px] md:top-[1700px]">
-        <h2 className="w-full text-center mb-5 text-4xl">Watch it in action</h2>
+        <h2 className="w-full text-center mb-5 text-3xl md:text-4xl">
+          Watch it in action
+        </h2>
         <div className="w-full md:w-3xl aspect-video bg-black rounded-3xl border-8 border-[#53674F]"></div>
       </div>
 
       <section className="max-w-screen-2xl min-w-screen absolute left-1/2 -translate-x-1/2 font-bold top-[1600px] md:top-[2300px]">
-        <div className="text-center text-4xl text-black">Our clubs</div>
+        <div className="text-center text-3xl md:text-4xl text-black">
+          Our clubs
+        </div>
         <div className="hidden lg:grid lg:grid-cols-6 lg:grid-rows-6 gap-4 h-[40rem] p-12 pt-5">
           <div className="col-span-2 row-span-2 bg-[#ADD2A4] rounded-2xl flex p-3 justify-between flex-col items-center"></div>
           <div className="col-span-2 row-span-6 bg-gradient-to-br from-[#add2a4a2] to-[#ADD2A4] rounded-2xl flex flex-col items-center justify-center">
@@ -218,7 +222,7 @@ export default function Home() {
           <div className="col-span-1 row-span-4 bg-[#ADD2A4] rounded-2xl flex items-center justify-around transition-all"></div>
         </div>
       </section>
-      <div className="h-[1716px] md:h-[2400px]]"></div>
+      <div className="h-[1716px] md:h-[2400px]"></div>
       <div className="w-full flex flex-col items-center justify-between">
         <Link
           href={"https://www.thisteenagelife.org/"}
